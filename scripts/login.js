7c4d21b2858dc7993b949cd2c3c3bf47ed498afc
Bernardo Mello <bernardomellogs@gmail.com>
2025-01-02 11:36:56 -0300

    let em login.js
    Trocando todos os var para let no arquivo login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,34 +1,47 @@
-var div_acesso = document.getElementById('acesso')
+let div_acesso = document.getElementById('acesso')
 
 document.getElementById('senha').addEventListener('keydown', function (event) {
+
     if (event.key === 'Enter') {
+
         acesso_login()
         event.preventDefault();
+
     }
+
 })
 
 verificar_login_automatico()
 
 function verificar_login_automatico() {
+
     try {
-        var acesso = JSON.parse(localStorage.getItem('acesso'))
+
+        let acesso = JSON.parse(localStorage.getItem('acesso'))
         if (acesso.acesso == 'Autorizado') {
+
             window.location.href = '/htmls/inicial.html'
+
         }
+
     } catch {
+
         console.log('Usuário deslogado')
+
     }
+
 }
 
 function alternar_telas(tela) {
 
     div_acesso.innerHTML = ''
 
-    var conteudo = ''
+    let conteudo = ''
 
     if (tela == 'novo') {
 
         conteudo = `
+
             <div style="display: flex; column; gap: 10px;">
                 <div style="display: flex; flex-direction: column; justify-content: left; flex; text-align: left;">
                     <label>Nome Completo</label>
@@ -52,11 +65,16 @@ function alternar_telas(tela) {
                 <div style="display: flex; flex-direction: column; justify-content: space-between; align-items: center; flex; text-align: center;">
                     <label style="cursor: pointer;" onclick="alternar_telas('login')">&times;</label>                        
                 </div>
-            </div>`
+            </div>
+            
+            `
 
     }
+
     if (tela == 'login') {
+
         conteudo = `
+
         <div id="acesso" style="display: flex; gap: 10px;">
             <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
                 <div style="display: flex; gap: 10px; justify-content: right; align-items: center;">
@@ -76,8 +94,10 @@ function alternar_telas(tela) {
                     <button style="background-color: #097fe6;" onclick="alternar_telas('novo')">Cadastre-se</button>
                 </div>
             </div>
-        </div>          
+        </div>    
+
         `
+
     }
 
     div_acesso.innerHTML = conteudo
@@ -85,106 +105,156 @@ function alternar_telas(tela) {
 }
 
 function acesso_login() {
+
     document.getElementById('acesso').style.display = 'none'
     document.getElementById('loading').style.display = 'flex'
 
-    var usuario = document.getElementById('usuario').value;
-    var senha = document.getElementById('senha').value;
+    let usuario = document.getElementById('usuario').value;
+    let senha = document.getElementById('senha').value;
 
     let url = 'https://script.google.com/macros/s/AKfycbx40241Ogk6vqiPxQ3RDjf4XURo3l_yG0x9j9cTNpeKIdnosEEewTnw7epPrc2Ir9EX/exec?bloco=' + usuario + '*' + senha
 
 
     if (usuario == "" || senha == "") {
+
         openPopup_v2("Senha e/ou usuário não informado(s)")
         document.getElementById('acesso').style.display = 'flex'
         document.getElementById('loading').style.display = 'none'
+
     } else {
+
         fetch(url)
+
             .then(response => {
+
                 if (!response.ok) {
+
                     throw new Error('Erro ao carregar os dados');
+
                 }
+
                 return response.json();
+
             })
+
             .then(data => {
 
                 switch (true) {
+
                     case data == "Usuário não cadastrado":
+
                         openPopup_v2(data)
                         break
+
                     case data == "Senha incorreta":
+
                         openPopup_v2(data)
                         break
+
                     case data.acesso == "Autorizado":
+
                         localStorage.setItem('acesso', JSON.stringify(data))
                         window.location.href = '/htmls/inicial.html'
                         break
+
                     default:
+
                         openPopup_v2('Falha interna. Entre em contato com o planejamento.')
+
                 }
+
                 document.getElementById('acesso').style.display = 'flex'
                 document.getElementById('loading').style.display = 'none'
 
             })
+
             .catch(error => {
+
                 console.error('Ocorreu um erro:', error);
+
             });
 
     }
+
 }
 
 function acesso_cadastro() {
+
     document.getElementById('acesso').style.display = 'none'
     document.getElementById('loading').style.display = 'flex'
 
-    var usuario = document.getElementById('usuario').value;
-    var senha = document.getElementById('senha').value;
-    var email = document.getElementById('email').value;
-    var nome_completo = document.getElementById('nome_completo').value;
-    var telefone = document.getElementById('telefone').value;
+    let usuario = document.getElementById('usuario').value;
+    let senha = document.getElementById('senha').value;
+    let email = document.getElementById('email').value;
+    let nome_completo = document.getElementById('nome_completo').value;
+    let telefone = document.getElementById('telefone').value;
 
 
     let url = 'https://script.google.com/macros/s/AKfycbx40241Ogk6vqiPxQ3RDjf4XURo3l_yG0x9j9cTNpeKIdnosEEewTnw7epPrc2Ir9EX/exec?bloco=CAD*' + usuario + '*' + senha + '*' + email + '*' + nome_completo + '*' + telefone
 
     if (usuario == "" || senha == "" || email == "") {
+
         openPopup_v2("Senha, usuário ou e-mail não informado(s)")
         document.getElementById('acesso').style.display = 'flex'
         document.getElementById('loading').style.display = 'none'
+
     } else {
+
         fetch(url)
+
             .then(response => {
+
                 if (!response.ok) {
+
                     throw new Error('Erro ao carregar os dados');
+
                 }
+
                 return response.json();
+
             })
+
             .then(data => {
 
                 switch (true) {
+
                     case data == "Usuário já cadastrado":
                         openPopup_v2(data)
                         break
+
                     case data == "Cadastro realizado!":
+
                         localStorage.setItem('acesso', JSON.stringify({
+
                             acesso: "Autorizado",
                             email: email,
                             nome_completo: nome_completo,
                             permissao: 'user',
                             telefone: telefone,
                             usuario: usuario,
+                            
                         }))
+
                         window.location.href = '/htmls/inicial.html'
                         break
+
                     default:
+
                         openPopup_v2('Falha interna. Entre em contato com o planejamento.', false)
+
                 }
+
                 document.getElementById('acesso').style.display = 'flex'
                 document.getElementById('loading').style.display = 'none'
 
             })
+
             .catch(error => {
+
                 console.error('Ocorreu um erro:', error);
+
             });
+            
     }
 
 }
