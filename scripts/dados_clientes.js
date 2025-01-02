2e82530ffad358208ecc8a8a03c3af7c2a0b3614
Bernardo Mello <bernardomellogs@gmail.com>
2025-01-02 10:44:32 -0300

    Let em dados_clientes.js
    Trocando todos os var para let no arquivo dados_clientes.js.

--- scripts/dados_clientes.js
+++ scripts/dados_clientes.js
@@ -1,4 +1,5 @@
-var lista = [
+let lista = [
+
     'cliente_selecionado',
     'estado',
     'cep',
@@ -16,11 +17,11 @@ var lista = [
     'tipo_de_frete',
     'condicoes',
     'garantia'
+
 ];
 
-var dados_clientes_provisorios = {}
+let dados_clientes_provisorios = {}
 
-// Processos para inicialização;
 vendedores_analistas()
 carregar_pagamentos()
 carregar_datalist_clientes()
@@ -28,61 +29,85 @@ recuperar_preenchido()
 
 async function carregar_datalist_clientes() {
 
-    var dados_clientes = {}
+    let dados_clientes = {}
+
     if (Object.keys(dados_clientes_provisorios).length > 0) {
+
         dados_clientes = dados_clientes_provisorios
 
     } else {
+
         dados_clientes = await recuperarDados('dados_clientes') || {};
+
     }
 
-    var items = []
+    let items = []
 
     for (cliente in dados_clientes) {
-        var dados = dados_clientes[cliente]
+
+        let dados = dados_clientes[cliente]
 
         items.push(dados.nome)
+
     }
 
-    var cliente_selecionado = document.getElementById('cliente_selecionado');
-    var suggestions = document.getElementById('suggestions');
+    let cliente_selecionado = document.getElementById('cliente_selecionado');
+    let suggestions = document.getElementById('suggestions');
 
     cliente_selecionado.addEventListener('input', function () {
-        var query = String(cliente_selecionado.value).toUpperCase()
+
+        let query = String(cliente_selecionado.value).toUpperCase()
         suggestions.innerHTML = '';
 
         if (query === '') return;
 
-        var filtrados = []
+        let filtrados = []
 
         items.forEach(it => {
+
             if (String(it).includes(query)) {
+
                 filtrados.push(it)
+
             }
+
         })
 
         filtrados.forEach(item => {
-            var div = document.createElement('div');
+
+            let div = document.createElement('div');
             div.textContent = item;
             div.classList.add('autocomplete-item');
             div.addEventListener('click', function () {
+
                 cliente_selecionado.value = item;
                 suggestions.innerHTML = '';
                 preencher('cliente_selecionado')
+
             });
+
             suggestions.appendChild(div);
+
         });
+
     });
 
     document.addEventListener('click', function (event) {
+
         if (!document.querySelector('.autocomplete-container').contains(event.target)) {
+
             suggestions.innerHTML = '';
+
         }
+
     });
+
 }
 
 function limpar_campos() {
+
     openPopup_v2('Limpar os campos?', true, `executar_limpar_campos()`)
+
 }
 
 function executar_limpar_campos() {
@@ -95,56 +120,74 @@ function executar_limpar_campos() {
 
     salvar_preenchido()
     remover_popup()
+
 }
 
 function carregar_pagamentos() {
-    var dados_pagamentos = JSON.parse(localStorage.getItem('dados_pagamentos'))
-    var condicoes = document.getElementById('condicoes')
+
+    let dados_pagamentos = JSON.parse(localStorage.getItem('dados_pagamentos'))
+    let condicoes = document.getElementById('condicoes')
 
     dados_pagamentos.forEach(function (item) {
-        var option = document.createElement('option')
+
+        let option = document.createElement('option')
         option.textContent = item
         condicoes.appendChild(option)
+
     })
+
 }
 
 
 function toggleTabela() {
-    var tabela = document.getElementById('tabela_dados_cliente');
+
+    let tabela = document.getElementById('tabela_dados_cliente');
     tabela.classList.toggle('show');
 
     if (tabela.classList.contains('show')) {
+
         menu_inferior.style.display = 'none'
         overlay.style.display = 'block';
+
     } else {
+
         menu_inferior.style.display = 'flex'
         overlay.style.display = 'none';
+
     }
+
 }
 
 function pagina_adicionar() {
+
     salvar_preenchido()
     window.location.href = '/htmls/adicionar.html'
+
 }
 
 function vendedores_analistas() {
-    var dados_vendedores = JSON.parse(localStorage.getItem('vendedores')) || {}
-    var vendedores = Object.keys(dados_vendedores)
 
-    var select = document.getElementById('vendedor')
+    let dados_vendedores = JSON.parse(localStorage.getItem('vendedores')) || {}
+    let vendedores = Object.keys(dados_vendedores)
+
+    let select = document.getElementById('vendedor')
 
     select.addEventListener('change', function () {
+
         atualizar_dados_vendedores()
         salvar_preenchido()
+
     })
 
     vendedores.forEach(function (vend_) {
-        var option = document.createElement('option')
+
+        let option = document.createElement('option')
         option.textContent = vend_
         select.appendChild(option)
+
     })
 
-    var dados_acesso = JSON.parse(localStorage.getItem('acesso'))
+    let dados_acesso = JSON.parse(localStorage.getItem('acesso'))
 
     document.getElementById('analista').textContent = dados_acesso.nome_completo
     document.getElementById('email_analista').textContent = dados_acesso.email
@@ -156,8 +199,9 @@ function vendedores_analistas() {
 
 function atualizar_dados_vendedores() {
 
-    var dados_vendedores = JSON.parse(localStorage.getItem('vendedores'))
-    var vendedor = document.getElementById('vendedor').value
+    let dados_vendedores = JSON.parse(localStorage.getItem('vendedores'))
+    let vendedor = document.getElementById('vendedor').value
+
     document.getElementById('email_vendedor').textContent = dados_vendedores[vendedor]['email']
     document.getElementById('telefone_vendedor').textContent = dados_vendedores[vendedor]['telefone']
 
@@ -166,47 +210,64 @@ function atualizar_dados_vendedores() {
 function incluir_listeners() {
 
     lista.forEach(function (item) {
+
         document.getElementById(item).addEventListener('input', function () {
             salvar_preenchido()
             total()
+
         })
+
     })
+
 }
 
 function salvar_preenchido() {
 
-    var orcamento_v2 = JSON.parse(localStorage.getItem('orcamento_v2')) || {}
+    let orcamento_v2 = JSON.parse(localStorage.getItem('orcamento_v2')) || {}
 
     if (orcamento_v2.lpu_ativa == 'MODALIDADE LIVRE') {
+
         total_v2()
+
     } else {
+
         total()
+
     }
 
-    var chamado_off = document.getElementById('chamado_off')
+    let chamado_off = document.getElementById('chamado_off')
 
     if (chamado_off.checked) {
+
         document.getElementById('contrato').style.display = 'none'
         document.getElementById('contrato').value = ''
+
     } else {
+
         document.getElementById('contrato').style.display = 'block'
+
     }
 
-    var orcamento_v2 = JSON.parse(localStorage.getItem('orcamento_v2')) || {}
+    orcamento_v2 = JSON.parse(localStorage.getItem('orcamento_v2')) || {}
 
-    var dados_analista = {
+    let dados_analista = {
+
         email: document.getElementById('email_analista').textContent,
         nome: document.getElementById('analista').textContent,
         telefone: document.getElementById('telefone_analista').textContent
+
     }
 
     if (orcamento_v2.id) {
+
         dados_analista.email = orcamento_v2.dados_orcam.email_analista
         dados_analista.telefone = orcamento_v2.dados_orcam.telefone_analista
         dados_analista.nome = orcamento_v2.dados_orcam.analista
+
     }
 
     orcamento_v2.dados_orcam = {
+
         analista: dados_analista.nome,
         estado: document.getElementById('estado').textContent,
         bairro: document.getElementById('bairro').textContent,
@@ -225,6 +286,7 @@ function salvar_preenchido() {
         telefone_vendedor: document.getElementById('telefone_vendedor').textContent,
         tipo_de_frete: document.getElementById('tipo_de_frete').value,
         vendedor: document.getElementById('vendedor').value,
+
     }
 
     localStorage.setItem('orcamento_v2', JSON.stringify(orcamento_v2));
@@ -232,61 +294,88 @@ function salvar_preenchido() {
 }
 
 function tipo_elemento(element) {
+
     if ('value' in element && (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA' || element.tagName === 'SELECT')) {
+
         return 'value';
+
     }
+
     return 'textContent';
+
 }
 
 
 function recuperar_preenchido() {
 
-    var orcamento_v2 = JSON.parse(localStorage.getItem('orcamento_v2')) || {}
-    var dados_orcam = orcamento_v2.dados_orcam || {}
+    let orcamento_v2 = JSON.parse(localStorage.getItem('orcamento_v2')) || {}
+    let dados_orcam = orcamento_v2.dados_orcam || {}
 
     if (dados_orcam.contrato == 'sequencial') {
+
         document.getElementById('chamado_off').checked = true
         document.getElementById('contrato').style.display = 'none'
+
     } else {
+
         document.getElementById('chamado_off').checked = false
         document.getElementById('contrato').style.display = 'block'
+
     }
 
     for (chave in dados_orcam) {
 
-        var info = dados_orcam[chave]
+        let info = dados_orcam[chave]
 
         if (document.getElementById(chave)) {
-            var elemento = document.getElementById(chave)
+
+            let elemento = document.getElementById(chave)
             if (tipo_elemento(elemento) == 'value') {
+
                 elemento.value = info
+
             } else {
+
                 elemento.textContent = info
+
             }
+
         }
 
     }
 
-    var levantamentos = document.getElementById('levantamentos')
+    let levantamentos = document.getElementById('levantamentos')
+
     if (orcamento_v2.levantamentos && levantamentos) {
-        var acumulado = ''
+
+        let acumulado = ''
 
         for (chave in orcamento_v2.levantamentos) {
-            var levantamento = orcamento_v2.levantamentos[chave]
 
-            var imagem = ''
+            let levantamento = orcamento_v2.levantamentos[chave]
+
+            let imagem = ''
 
             if (formato(levantamento.formato) == 'PDF') {
+
                 imagem = 'pdf'
+
             } else if (formato(levantamento.formato) == 'IMAGEM') {
+
                 imagem = 'imagem'
+
             } else if (formato(levantamento.formato) == 'PLANILHA') {
+
                 imagem = 'excel2'
+
             } else {
+
                 imagem = 'anexo'
+
             }
 
             acumulado += `
+
             <div style="display: flex; gap: 10px; align-items: center; justify-content: center;">
                 <div style="align-items: center; width: max-content; font-size: 0.7em; display: flex; justify-content; left; box-shadow: 2px 2px #94a0ab; background-color: #e9e9e9; color: #555; padding: 5px; margin:5px; border-radius: 5px;">
                     <img src="${imagem}.png" style="width: 3vw;">
@@ -294,16 +383,20 @@ function recuperar_preenchido() {
                 </div>
                 <label style="text-decoration: underline; font-size: 0.7em; cursor: pointer;" onclick="excluir_levantamento('${chave}')">Excluir</label>
             </div>
+
             `
+
         }
 
         levantamentos.innerHTML = acumulado
+
     }
 
 }
 
 function excluir_levantamento(chave) {
-    var orcamento_v2 = JSON.parse(localStorage.getItem('orcamento_v2')) || {}
+
+    let orcamento_v2 = JSON.parse(localStorage.getItem('orcamento_v2')) || {}
 
     if (orcamento_v2.levantamentos) {
 
@@ -314,62 +407,83 @@ function excluir_levantamento(chave) {
         recuperar_preenchido()
 
     }
+
 }
 
 function formatarCEP(cep) {
+
     cep = String(cep);
     cep = cep.replace(/\D/g, '');
     cep = cep.replace(/^(\d{5})(\d{3})/, '$1-$2');
     return cep;
+
 }
 
 function formatarCNPJ_CPF(documento) {
+
     documento = documento.replace(/\D/g, '');
 
     if (documento.length === 11) {
+
         return documento.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 
     } else if (documento.length === 14) {
+
         return documento.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
 
     } else {
+
         return 0;
+
     }
+
 }
 
 async function preencher(campo) {
 
-    var dados_clientes = {}
-    var dados_com_chave_nome = {}
-    var termo = document.getElementById(campo).value
+    let dados_clientes = {}
+    let dados_com_chave_nome = {}
+    let termo = document.getElementById(campo).value
 
     if (Object.keys(dados_clientes_provisorios).length > 0) {
+
         dados_clientes = dados_clientes_provisorios
 
     } else {
+
         dados_clientes = await recuperarDados('dados_clientes') || {};
+
     }
 
     if (campo == 'cliente_selecionado') {
 
         for (cnpj in dados_clientes) {
-            var infos = dados_clientes[cnpj]
+
+            let infos = dados_clientes[cnpj]
             dados_com_chave_nome[infos.nome] = infos
+
         }
+
         dados_clientes = dados_com_chave_nome
+
     } else {
+
         termo = formatarCNPJ_CPF(termo)
+
     }
 
-    var lista = ['estado', 'cep', 'bairro', 'cidade']
+    let lista = ['estado', 'cep', 'bairro', 'cidade']
 
     if (dados_clientes[termo]) {
 
         if (campo == 'cnpj') {
+
             document.getElementById('cliente_selecionado').value = dados_clientes[termo].nome
 
         } else if (campo == 'cliente_selecionado') {
+
             document.getElementById('cnpj').value = dados_clientes[termo]?.cnpj || ''
+
         }
 
         lista.forEach(item => {
@@ -381,10 +495,13 @@ async function preencher(campo) {
     } else {
 
         if (campo == 'cnpj') {
+
             document.getElementById('cliente_selecionado').value = ''
 
         } else if (campo == 'cliente_selecionado') {
+
             document.getElementById('cnpj').value = dados_clientes[termo]?.cnpj || ''
+
         }
 
         lista.forEach(item => {
@@ -392,6 +509,7 @@ async function preencher(campo) {
             document.getElementById(item).textContent = 'Preencha o CNPJ ou Nome do Cliente'
 
         })
+        
     }
 
     salvar_preenchido()
