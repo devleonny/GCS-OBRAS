a3a687596b6827f8af75da80ade881cf826a0b95
gabisantosdev <130710570+gabisantosdev@users.noreply.github.com>
2025-04-15 09:44:01 -0300

    refatorando ifs

--- status.js
+++ status.js
@@ -860,7 +860,9 @@ async function definir_campo(elemento, div, id) {
 
             if (historico.operacao == 'entrada') {
                 dic_quantidades[estoque] += historico.quantidade
-            } else if (historico.operacao == 'saida') {
+            }
+
+            if (historico.operacao == 'saida') {
                 dic_quantidades[estoque] -= historico.quantidade
             }
         }
@@ -975,9 +977,7 @@ async function salvar_pedido(chave) {
         aviso_campo_branco.style.display = "flex"
 
         setTimeout(() => {
-
             aviso_campo_branco.style.display = "none"
-
         }, 3000);
 
         return
@@ -1034,9 +1034,7 @@ async function salvar_notas(chave) {
         aviso_campo_branco.style.display = "flex"
 
         setTimeout(() => {
-
             aviso_campo_branco.style.display = "none"
-
         }, 3000);
 
         return
@@ -1829,14 +1827,15 @@ function mostrar_painel() {
     let overlay_de_custos = document.getElementById('overlay_de_custos')
 
     if (painel_custos) {
-        if (painel_custos.style.display == 'flex') {
-            painel_custos.style.display = 'none'
-            overlay_de_custos.style.display = 'none'
-
-        } else {
+        if (painel_custos.style.display !== 'flex') {
             painel_custos.style.display = 'flex'
             overlay_de_custos.style.display = 'block'
+            return;
         }
+
+        painel_custos.style.display = 'none'
+        overlay_de_custos.style.display = 'none'
+
     }
 }
 
