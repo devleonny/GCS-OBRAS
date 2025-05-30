a817d8e52d090333271ec4ecd87ad314afc73bce
devleonny <166626175+devleonny@users.noreply.github.com>
2025-05-30 17:11:52 -0300

    fix: icone de qualidade

--- pagamentos.js
+++ pagamentos.js
@@ -26,19 +26,7 @@ async function consultar_pagamentos() {
     let dados_categorias = await recuperarDados('dados_categorias')
     if (!dados_categorias) {
         dados_categorias = await receber('dados_categorias')
-        inserirDados(dados_categorias, 'dados_categorias')
-    }
-
-    // timestamp para dados_setores: Sempre atualizado;
-    let timestamps = JSON.parse(localStorage.getItem('timestamps')) || {}
-    let timestamp_atual_setores = await ultimo_timestamp('dados_setores')
-
-    if (timestamp_atual_setores.timestamp > (timestamps?.dados_setores || 0)) {
-        dados_setores = await lista_setores()
-        timestamps.dados_setores = timestamp_atual_setores.timestamp
-        localStorage.setItem('timestamps', JSON.stringify(timestamps))
-    } else {
-        dados_setores = JSON.parse(localStorage.getItem('dados_setores')) || {}
+        await inserirDados(dados_categorias, 'dados_categorias')
     }
 
     let orcamentos = await recuperarDados('dados_orcamentos') || {};
@@ -211,7 +199,6 @@ async function consultar_pagamentos() {
         `
     div_pagamentos.innerHTML = elementus
 
-
 }
 
 function iconePagamento(status) {
@@ -237,8 +224,11 @@ function iconePagamento(status) {
         case status.includes('Excluído'):
             icone = 'alerta'
             break
-        case status.includes('ATRASADO'):
+        case status.includes('Aguardando aprovação da Qualidade'):
             icone = 'atrasado'
+            break
+        case status.includes('ATRASADO'):
+            icone = 'qualidade2'
             break
         case status == 'Aguardando aprovação da Diretoria':
             icone = 'diretoria'
@@ -775,7 +765,7 @@ async function modal_editar_pagamento(id, indice) {
 
     let opcoes = ''
     Object.entries(dados_categorias).forEach(([codigo, categoria]) => {
-        opcoes += `<option data-codigo="${codigo}" ${categoriaAtual == codigo ? 'selected': ''}>${categoria}</option>`
+        opcoes += `<option data-codigo="${codigo}" ${categoriaAtual == codigo ? 'selected' : ''}>${categoria}</option>`
     })
 
     let acumulado = `
