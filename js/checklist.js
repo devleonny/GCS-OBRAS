036723ac28685fe33a63f289b4a623cc6868fdf8
devleonny <166626175+devleonny@users.noreply.github.com>
2025-12-08 11:02:25 -0300

    fix: diversos

--- js/checklist.js
+++ js/checklist.js
@@ -1111,7 +1111,7 @@ async function registrarChecklist(codigo) {
 
     // Bloqueio por excesso de quantidade;
     quantidadeRealizadoItem = 0
-    quantidadeItem = orcamento?.dados_composicoes?.[codigo]?.qtde || orcamento?.checklist?.avulso?.[codigo].qtde || 0
+    quantidadeItem = orcamento?.checklist?.qReal[codigo]?.qtde || orcamento?.dados_composicoes?.[codigo]?.qtde || orcamento?.checklist?.avulso?.[codigo].qtde || 0
 
     for (const [idLancamento, dados] of Object.entries(itens)) {
 
