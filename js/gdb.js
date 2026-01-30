c81efc89987d2357aedd3760e31f2d06ae5464f3
devleonny <166626175+devleonny@users.noreply.github.com>
2026-01-30 08:47:55 -0300

    em andamento...

--- js/gdb.js
+++ js/gdb.js
@@ -1,12 +1,11 @@
 const nomeBase = 'GCS v2'
 const versao = 1
 
-const stores = {
-    dados_setores: { keyPath: 'usuario' },
-    dados_orcamentos: { keyPath: 'id' },
-}
+function criarBases(stores = null) {
 
-function criarBases() {
+    if (!stores)
+        return
+
     const request = indexedDB.open(nomeBase, versao)
 
     request.onupgradeneeded = e => {
@@ -14,13 +13,136 @@ function criarBases() {
 
         Object.entries(stores).forEach(([nome, config]) => {
             if (!db.objectStoreNames.contains(nome)) {
-                db.createObjectStore(nome, config)
+                const store = db.createObjectStore(nome, config)
+
+                store.createIndex(
+                    'timestamp',
+                    'timestamp',
+                    { unique: false }
+                )
             }
         })
     }
 }
 
-function inserirDadosv2(dados, base) {
+async function atualizarGCS(resetar) {
+
+    if (emAtualizacao) return
+
+    mostrarMenus(true)
+
+    emAtualizacao = true
+    sincronizarApp()
+
+    const basesAuxiliares = {
+        'informacoes': { keyPath: 'id' },
+        'dados_setores': { keyPath: 'usuario' },
+        'empresas': { keyPath: 'id' },
+        'sistemas': { keyPath: 'id' },
+        'prioridades': { keyPath: 'id' },
+        'correcoes': { keyPath: 'id' },
+        'tipos': { keyPath: 'id' },
+        'veiculos': { keyPath: 'id' },
+        'motoristas': { keyPath: 'id' },
+        'dados_estoque': { keyPath: 'id' },
+        'custo_veiculos': { keyPath: 'id' },
+        'dados_composicoes': { keyPath: 'id' },
+        'dados_clientes': { keyPath: 'id' },
+        'dados_categorias_AC': { keyPath: 'id' },
+        'dados_manutencao': { keyPath: 'id' },
+        'dados_ocorrencias': { keyPath: 'id' },
+        'dados_orcamentos': { keyPath: 'id' },
+        'lista_pagamentos': { keyPath: 'id' },
+        'tags_orcamentos': { keyPath: 'id' },
+        'departamentos_AC': { keyPath: 'codigo' }
+    }
+
+    criarBases(basesAuxiliares)
+
+    const status = { total: Object.keys(basesAuxiliares).length + 1, atual: 1 }
+
+    sincronizarApp(status)
+
+    status.atual++
+
+    for (const base of Object.keys(basesAuxiliares)) {
+
+        sincronizarApp(status)
+
+        await sincronizarDados({ base, resetar })
+
+        status.atual++
+
+        // A tabela é a primeira: atualiza os dados da empresa atual antes da tabela de clientes;
+        if (base == 'dados_setores') {
+            const existente = obterPorID({ chave: 'usuario', base: 'dados_setores' })
+
+            if (!existente) continue
+
+            acesso = existente
+            localStorage.setItem('acesso', JSON.stringify(existente))
+
+        }
+
+    }
+
+    sincronizarApp({ remover: true })
+    emAtualizacao = false
+    await executar(funcaoTela)
+
+}
+
+async function sincronizarDados({ base, resetar = false }) {
+
+    let timestamp = 0
+
+    if (!resetar) {
+        const request = indexedDB.open(nomeBase, versao)
+
+        request.onupgradeneeded = e => {
+            const db = e.target.result
+            const tx = db.transaction(base)
+            const store = tx.objectStore(base)
+            const index = store.index('timestamp')
+
+            index.openCursor(null, 'prev').onsuccess = e => {
+                const c = e.target.result
+                if (c) timestamp = c.value.timestamp
+            }
+        }
+    }
+
+
+    try {
+
+        const response = await fetch(`${api}/dados`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ chave, timestamp })
+        })
+
+        if (!response.ok) {
+            throw new Error(`Erro ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        if (data?.mensagem) {
+            console.log(data.mensagem)
+            return {}
+        }
+
+        return inserirDados(data)
+
+    } catch (err) {
+        console.log(err.message)
+        return {}
+    }
+
+}
+
+
+function inserirDados(dados, base) {
 
     const request = indexedDB.open(nomeBase, versao)
 
@@ -30,7 +152,9 @@ function inserirDadosv2(dados, base) {
         const tx = db.transaction(base, 'readwrite')
         const store = tx.objectStore(base)
 
-        store.put(dados)
+        for (const d of Object.values(dados)) {
+            store.put(d)
+        }
 
     }
 
